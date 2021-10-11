5a986c2e11f040d3063ef78c3d1b12e276d60e54
zkut <frank.ferrini@gmail.com>
2021-10-11 12:54:49 +0200

    primo commit

---
+++ src/main/java/com/example/demogit/DemogitApplication.java
@@ -0,0 +1,13 @@
+package com.example.demogit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemogitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemogitApplication.class, args);
+	}
+
+}
